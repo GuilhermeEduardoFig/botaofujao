e219d9b3dc8887830ab33aff9d81174400b253f1
Guilherme Eduardo <l38259905@gmail.com>
2026-04-27 00:39:40 -0300

    new2

--- script/script.js
+++ script/script.js
@@ -49,7 +49,13 @@ function tocarMusica(){
     }, 200);
 }
 
-const texto = `É Minha gata o momento que vc tanto esperou chegou, quando vc ler isso mto provavelmente eu já coloquei uma linda aliança em seu dedo. No momento em que estou escrevendo isso a gente tem 6 meses e 19 dias juntos e eu só sei que  a cada dia e hora que passe eu só fico mais apaixonado por vc e sinceramente só tenho mais certeza que vc é a mulher que eu quero me casar e levar para a vida toda. Muito Obrigada por ser tão atenciosa, carinhosa e muitooooo paciente, eu não tenho nada a reclamar, afinal vc é essa menina maravilhosamente linda, muito incrível e absurdamente cheirosa e é isso meu amor, eu prometo te amar pra toda eternidade e em todas as vidas minha princesa ❤️💞`;
+const audio = document.getElementById("musica");
+
+document.addEventListener("click", () => {
+    audio.play();
+}, { once: true });
+
+const texto = `É Minha gata o momento que vc tanto esperou chegou, quando vc ler isso mto provavelmente eu já coloquei uma linda aliança em seu dedo. No momento em que estou escrevendo isso a gente tem 6 meses e 19 dias juntos e eu só sei que  a cada dia e hora que passe eu só fico mais apaixonado por vc e sinceramente só tenho mais certeza que vc é a mulher que eu quero me casar e levar para a vida toda.   Muito Obrigada por ser tão atenciosa, carinhosa e muitooooo paciente comigo, sei que demorei bastante até fazer isso acontecer, porém não significa que eu não te amo ou não queira um futuro ao seu lado, pois na verdade tudo que eu mais quero é viver essa vida ao seu lado e te amar cada vez mais e a partir de agora eu declaro que seu contrato comigo foi renovado pra toda a eternidade e ele não pode ser revogado, muito obrigado de coração minha gatinha, eu prometo te amar pra toda eternidade e em todas as vidas minha princesa ❤️💞`;
 
 let paginas = [];
 let paginaAtual = 0;
