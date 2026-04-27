1e310eca8931ae7a152dcf58c761363bc59ba6cd
Guilherme Eduardo <l38259905@gmail.com>
2026-04-27 00:02:27 -0300

    new

---
+++ script/script.js
@@ -0,0 +1,83 @@
+        function fuja(){
+    var botao = document.getElementById("nao");
+    var painel = document.querySelector(".painel");
+
+    var larguraPainel = painel.clientWidth;
+    var alturaPainel = painel.clientHeight;
+
+    var maxX = larguraPainel - botao.offsetWidth;
+    var maxY = alturaPainel - botao.offsetHeight;
+
+    var x = Math.random() * maxX;
+    var y = Math.random() * maxY;
+
+    botao.style.left = x + "px";
+    botao.style.top = y + "px";
+}
+
+let musicaTocando = false;
+
+function abrirCarta(el){
+    if(!el.classList.contains("aberta")){
+        el.classList.add("aberta");
+        iniciarCarta();
+
+        // 🎶 toca exatamente no clique
+        if(!musicaTocando){
+            tocarMusica();
+            musicaTocando = true;
+        }
+    }
+}
+
+function tocarMusica(){
+    const audio = document.getElementById("musica");
+
+    audio.currentTime = 0;
+    audio.volume = 0;
+
+    audio.play().catch(()=>{});
+
+    let vol = 0;
+    const fade = setInterval(()=>{
+        if(vol < 1){
+            vol += 0.05;
+            audio.volume = vol;
+        } else {
+            clearInterval(fade);
+        }
+    }, 200);
+}
+
+const texto = `É Minha gata o momento que vc tanto esperou chegou, quando vc ler isso mto provavelmente eu já coloquei uma linda aliança em seu dedo. No momento em que estou escrevendo isso a gente tem 6 meses e 19 dias juntos e eu só sei que  a cada dia e hora que passe eu só fico mais apaixonado por vc e sinceramente só tenho mais certeza que vc é a mulher que eu quero me casar e levar para a vida toda. Muito Obrigada por ser tão atenciosa, carinhosa e muitooooo paciente, eu não tenho nada a reclamar, afinal vc é essa menina maravilhosamente linda, muito incrível e absurdamente cheirosa e é isso meu amor, eu prometo te amar pra toda eternidade e em todas as vidas minha princesa ❤️💞`;
+
+let paginas = [];
+let paginaAtual = 0;
+
+function dividirTexto(){
+    const limite = 394; // quantidade de caracteres por página
+    for(let i = 0; i < texto.length; i += limite){
+        paginas.push(texto.substring(i, i + limite));
+    }
+}
+
+function mostrarPagina(){
+    document.getElementById("paginas").innerText = paginas[paginaAtual];
+}
+
+function proximaPagina(){
+    if(paginaAtual < paginas.length - 1){
+        paginaAtual++;
+        mostrarPagina();
+    }
+}
+
+function iniciarCarta(){
+    dividirTexto();
+    mostrarPagina();
+}
+
+function abrirCarta(el){
+    el.classList.add("aberta");
+    iniciarCarta();
+}
